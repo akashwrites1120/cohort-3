a7f5758c9595a87442c9687222c7f9d58652298d
Akash Maity <akashmaity4452@gmail.com>
2025-06-06 20:02:45 +0530

    Notification Count

--- Week 09 - React/week-9-offline/src/App.jsx
+++ Week 09 - React/week-9-offline/src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import PostComponent from './PostComponent'
-import ToggleMessage from './Conditional Rendering/ToggleMessage'
+import React from "react";
+import PostComponent from "./PostComponent";
+import ToggleMessage from "./Conditional Rendering/ToggleMessage";
+import NotificationCount from "./Conditional Rendering/NotificationCount";
 
 function App() {
   return (
     <div>
       {/* <PostComponent /> */}
-      <ToggleMessage />
+      {/* <ToggleMessage /> */}
+      <div style={{ background: "#dfe6e9", height: "100vh" }}>
+        <NotificationCount />
+        <NotificationCount />
+        <NotificationCount />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Week 09 - React/week-9-offline/src/Conditional Rendering/ToggleMessage.jsx
+++ Week 09 - React/week-9-offline/src/Conditional Rendering/ToggleMessage.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 function ToggleMessage() {
-    const [isVisible, setVisible] = useState(false)
+  const [isVisible, setVisible] = useState(false);
   return (
-    <div>
-        <button onClick={()=>setVisible(!isVisible)}>Toggle Message</button>
-        {isVisible && <p>This message is conditionally rendered</p> }
+    <div style={{ background: "#dfe6e9", height: "100vh" }}>
+      <button onClick={() => setVisible(!isVisible)}>Toggle Message</button>
+      {isVisible && <p>This message is conditionally rendered</p>}
     </div>
-  )
+  );
 }
 
-export default ToggleMessage
+export default ToggleMessage;
