a0235ecf23ffbd096f21d57d4df84afc70ed5306
Akash Maity <akashmaity4452@gmail.com>
2025-06-07 13:17:30 +0530

    Timer useEffect

--- Week 09 - React/week-9-offline/src/App.jsx
+++ Week 09 - React/week-9-offline/src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import PostComponent from "./03-useState/PostComponent";
-import ToggleMessage from "./02-Conditional Rendering/ToggleMessage";
-import NotificationCount from "./02-Conditional Rendering/NotificationCount";
+import ToggleMessage from "./07-Conditional Rendering/ToggleMessage";
+import NotificationCount from "./05-useEffect/NotificationCount";
 import Posts from "./03-useState/Posts";
+import Timer from "./05-useEffect/Timer";
 
 function App() {
   return (
     <div>
       {/* <PostComponent /> */}
-      <Posts />
+      {/* <Posts /> */}
       {/* <ToggleMessage /> */}
       {/* <div style={{ background: "#dfe6e9", height: "100vh" }}>
         <NotificationCount />
         <NotificationCount />
         <NotificationCount />
       </div> */}
+
+      <Timer />
 
 
     </div>
