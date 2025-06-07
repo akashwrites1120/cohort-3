e2e6b5c0edd6f81749e4588ef30971cb3435e403
Akash Maity <akashmaity4452@gmail.com>
2025-06-07 12:25:11 +0530

    Posts.jsx

---
+++ Week 09 - React/week-9-offline/src/03-useState/Posts.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import PostComponent from "./PostComponent";
+
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  const postComponent = posts.map((post) => (
+    <PostComponent
+      name={post.name}
+      subtitle={post.subtitle}
+      time={post.time}
+      image={post.image}
+      description={post.description}
+    />
+  ));
+
+  function addPost() {
+    setPosts([
+      ...posts,
+      {
+        name: "harkirat",
+        subtitle: "10000 followers",
+        time: "2m ago",
+        image:
+          "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+        description:
+          "What to know how to win big? Check out how these folks won $6000 in bounties.",
+      },
+    ]);
+  }
+  return (
+    <div style={{ background: "#dfe6e9", height: "100vh" }}>
+      <button onClick={addPost}>Add Post</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>{postComponent}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
--- Week 09 - React/week-9-offline/src/App.jsx
+++ Week 09 - React/week-9-offline/src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import PostComponent from "./PostComponent";
-import ToggleMessage from "./Conditional Rendering/ToggleMessage";
-import NotificationCount from "./Conditional Rendering/NotificationCount";
+import PostComponent from "./03-useState/PostComponent";
+import ToggleMessage from "./02-Conditional Rendering/ToggleMessage";
+import NotificationCount from "./02-Conditional Rendering/NotificationCount";
+import Posts from "./03-useState/Posts";
 
 function App() {
   return (
     <div>
       {/* <PostComponent /> */}
+      <Posts />
       {/* <ToggleMessage /> */}
-      <div style={{ background: "#dfe6e9", height: "100vh" }}>
+      {/* <div style={{ background: "#dfe6e9", height: "100vh" }}>
         <NotificationCount />
         <NotificationCount />
         <NotificationCount />
-      </div>
+      </div> */}
+
+
     </div>
   );
 }
